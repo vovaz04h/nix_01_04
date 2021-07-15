e0564b3a507eb2dc959292eb0131a2cf893a6cfe
Vladimir Ukalitsky <vovaz04h@gmail.com>
2021-07-15 22:17:53 +0300

    concurently get posts

--- main.go
+++ main.go
@@ -1,4 +1,53 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+const (
+	POSTS_COUNT = 100
+	BASE_URL    = "https://jsonplaceholder.typicode.com/posts/"
+)
+
+var (
+	wg sync.WaitGroup
+)
+
+func getPost(id int) {
+
+	defer wg.Done()
+
+	resp, err := http.Get(BASE_URL + strconv.Itoa(id))
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println(string(body))
+
+}
+
 func main() {
+
+	for postID := 1; postID <= POSTS_COUNT; postID++ {
+		wg.Add(1)
+		go getPost(postID)
+	}
+
+	wg.Wait()
 }
